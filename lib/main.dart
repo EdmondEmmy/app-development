43fec20aead393b9f3212551d556e0df936aa4ac
EdmondEmmy <edmondemmy@gmail.com>
2023-11-05 15:48:15 +0300

    added main function

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,11 @@ import 'package:rsa_encrypt/rsa_encrypt.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:pointycastle/export.dart'as pointyexport;
+
+void main(){
+
+}
+
+
+
+}
