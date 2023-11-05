80452e7412e44970f0cfe6ab979dbb165da215ef
EdmondEmmy <edmondemmy@gmail.com>
2023-11-05 15:39:33 +0300

    added required packages to pubspec.yaml

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:encrypt/encrypt.dart';
+import 'package:rsa_encrypt/rsa_encrypt.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:pointycastle/export.dart'as pointyexport;
