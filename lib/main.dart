f6f430192669f169e9b64f86f596a538e7912684
AlinaitweH <alinaitweharriet@gmail.com>
2023-11-08 18:56:31 +0300

    added function to generate keys

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,30 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:pointycastle/export.dart'as pointyexport;
 
+final TextEditingController decryptedTextController = TextEditingController();
+final TextEditingController controller = TextEditingController();
+final TextEditingController encryptedTextController = TextEditingController();
+
+const storage = FlutterSecureStorage();
+
+Future<void> generateAndStoreKeys() async {
+  final rsaKeyHelper = RsaKeyHelper();
+  final rsaKeyPair =
+      await rsaKeyHelper.computeRSAKeyPair(rsaKeyHelper.getSecureRandom());
+
+  final publicKey = rsaKeyPair.publicKey as pointyexport.RSAPublicKey;
+  final privateKey = rsaKeyPair.privateKey as pointyexport.RSAPrivateKey;
+
+  final publicKeyPEM = rsaKeyHelper.encodePublicKeyToPemPKCS1(publicKey);
+  final privateKeyPEM = rsaKeyHelper.encodePrivateKeyToPemPKCS1(privateKey);
+
+  await storage.write(key: 'publicKey', value: publicKeyPEM);
+  await storage.write(key: 'privateKey', value: privateKeyPEM);
+}
+
 void main(){
 
 }
 
 
 
-}
