ab3d794dc0a51458d47917c27d55c6dd485436ab
musingura pascal <pascalmusingura@gmail.com>
2023-11-10 12:41:05 +0300

    added widgets class

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ Future<pointyexport.RSAPrivateKey?> getPrivateKey() async {
   return null;
 }
 void main(){
-
+    runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -71,5 +71,78 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class TextEntryWidgetEncryptMessage extends StatefulWidget {
+  const TextEntryWidgetEncryptMessage({super.key});
 
+  @override
+  _TextEntryWidgetEncryptMessageState createState() =>
+      _TextEntryWidgetEncryptMessageState();
+}
+
+
+class TextArea extends StatelessWidget {
+  const TextArea({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: TextField(
+                  controller: decryptedTextController,
+                  maxLines: 4,
+                  enabled: false,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () async {
+                      await generateAndStoreKeys();
+
+                      // Display a toast message to indicate that keys were generated
+                      Fluttertoast.showToast(
+                        msg: 'Keys were generated and stored',
+                        gravity: ToastGravity.BOTTOM,
+                        backgroundColor: Colors.black,
+                        textColor: Colors.white, //
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green),
+                    child: const Text('Generate And Store Keys')
+                ),
+                const SizedBox(width: 16.0),
+                ElevatedButton(
+                  onPressed: () {
+                    // Call the resetTextFields function to clear text fields
+                    resetTextFields();
+                  },
+                  child: const Text('Reset'),
+                ),
+
+              ],
+            ),
+          ],
+        )
+    );
+  }
+  void resetTextFields(){
+    controller.text = '';
+    encryptedTextController.text = '';
+    decryptedTextController.text = '';
+  }
+}
 
